344cc074aab5bb7cc5bfe9a1b6b13e2acb0f8b10
Shreya A S <shreyaa1@in.ibm.com>
2020-04-03 11:47:52 +0530

    dsfdsf

---
+++ Sam.java
@@ -0,0 +1,11 @@
+git.txt  git2.java  sam.java  Trail.java  Trail.txt  Trail2.java
+
+
+public class Sam {
+int i;
+    public static void main(String[] args) {
+	// write your code here
+	for(int i=0;i<10;i++)
+        System.out.println("hi f10");
+}
+}
