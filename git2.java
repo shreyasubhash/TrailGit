bf51a51cbbcfac879cbfc4d90f4f58b5473a54fc
Shreya A S <shreyaa1@in.ibm.com>
2020-04-03 11:01:09 +0530

    last didnt trigger so added

---
+++ git2.java
@@ -0,0 +1,12 @@
+
+
+public class Trail {
+int i;
+    public static void main(String[] args) {
+	// write your code here
+	for(int i=0;i<10;i++){
+	if(i<10)
+        System.out.println("hi f10");
+}
+}
+}
